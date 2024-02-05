6e63a0f16cce610398b7ad47ad223175c595426b
SudiptaAdhikaryJoy <sudiptaadhikaryjoy@gmail.com>
2024-02-06 02:24:31 +0600

    fix: 🐛 some style related bug fixes and solve

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 import React from 'react';
+import { ModeToggle } from './ModeToggle';
 
 const Navbar = () => {
     return (
-        <div className=' w-full relative flex items-center justify-center max-w-2xl mx-auto px-5 py-4  '>
+        <nav className=' w-full relative flex items-center justify-between max-w-2xl mx-auto px-5 py-4  '>
             <Link href={'/'} className=' font-bold text-4xl text-black '>
 Sudipta<span className=' text-blue-400 '>Blog</span>
             </Link>
-        </div>
+            <ModeToggle/>
+        </nav>
     );
 };
 
